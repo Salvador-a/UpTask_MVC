9bb4b24ed20636b5013f8a673780a1e5932ebacf
Salvador-a <salvador.sar614@outlook.com>
2024-01-02 14:38:46 -0600

    Empesanso creacion del repo

---
+++ includes/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', '51573M45', '');
+
+
+if (!$db) {
+    echo "Error: No se pudo conectar a MySQL.";
+    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
+    exit;
+}
