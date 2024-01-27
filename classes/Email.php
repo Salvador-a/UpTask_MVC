55d783691200311bb1eb4984a8e4f23cbdfb292e
Salvador-a <salvador.sar614@outlook.com>
2024-01-26 20:59:56 -0600

    Configuracion de PHPMaeiler con problemas

--- classes/Email.php
+++ classes/Email.php
@@ -60,40 +60,7 @@ class Email {
 
     }
 
-    // public function enviarInstrucciones() {
-
-    //     $mail = new PHPMailer();
-    //     $mail->isSMTP();
-    //     $mail->Host       = $_ENV['EMAIL_HOST'];
-    //     $mail->SMTPAuth   = true;
-    //     $mail->Username   = $_ENV['EMAIL_USER'];
-    //     $mail->Password   = $_ENV['EMAIL_PASS'];
-    //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    //     $mail->Port       = $_ENV['EMAIL_PORT'];
-
-    //     $mail->setFrom('pruebasphpmvc@gmail.com');
-    //     $mail->addAddress($this->email);
-    //     $mail->Subject = 'Reestablece tu password';
-
-    //     // Set HTML
-    //     $mail->isHTML(TRUE);
-    //     $mail->CharSet = 'UTF-8';
-
-    //      $contenido = '<html>';
-    //      $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-    //      $contenido .= "<p>Presiona aquí: <a href='" .  $_ENV['APP_URL']  . "/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
-    //      $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
-    //      $contenido .= '</html>';
-         
-    //      $mail->Body = $contenido;
-
-    //      //Enviar el mail
-    //      if ($this->email) {
-    //         $mail->send();
-    //     }
-
-    // }
-
+    
     
 
 }
