15d9e5c4be0b8bb99e18f0522c58eddab150d43e
Salvador-a <salvador.sar614@outlook.com>
2024-01-31 14:02:34 -0600

    Creadno la vista de Dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -7,6 +7,7 @@ use MVC\Router;
 class dashboardController {
     public static function index(Router $router) {
         
+        session_start();
         
         
         $router->render('dashboard/index', [
---
+++ views/templates/sidebar.php
@@ -0,0 +1,11 @@
+<aside class="siderbar">
+    <h2>UpTask</h2>
+
+    <nav class="siderbar-nav">
+        <a href="/dashboard">Proyectos</a>
+        <a href="/crear-proyecto">Crear Proyecto</a>
+        <a href="/perfil">Perfil</a>
+        
+    </nav>
+
+</aside>
